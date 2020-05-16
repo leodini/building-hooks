3e81b1b4c759e705d6ccfa465e6cc46a1b83008d
leodini <leodini@hotmail.com>
2020-05-15 22:56:06 -0300

    my own hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
+function useFetch(url, defaultResponse) {
+  const [ data, setData ] = useState(defaultResponse)
+  const [ isLoading, setIsLoading ] = useState(true)
+
+  async function getDataFromApi(url){
+    try{
+      const res = await fetch(url)
+      const data = await res.json()
+      console.log(data)
+      setData(data)
+      setIsLoading(false)
+    } catch(e){
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    getDataFromApi(url)
+    return () => {
+      
+    }
+  }, [url])
+
+  return {data, isLoading}
+}
+
 function App() {
+
+  const user = 'leodini'
+  const apiEndpoint = `https:api.github.com/users/${user}`
+
+  const { data, isLoading } = useFetch(apiEndpoint, {isLoading: true, data: null})
+
+  if(!data || isLoading){
+    return 'LOADING...'
+  }
+
+  const { login, avatar_url } = data
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h3>name: {login}</h3>
+        <img src={avatar_url} alt="avatar"/>
+      </div>
     </div>
   );
 }
